64598117205c228e6ead01e9e4ddf5ac38e7da45
pete george <pete@georgetd.com>
2016-02-12 14:15:09 +0000

    Initial checkin of cipractical project

---
+++ cipractical/src/main/java/calculator/GoogleCalculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class GoogleCalculator {
+	String calculationOutput=null;
+
+	public void submitEquation(String equation) {
+		if (equation.contains("+")){
+			String[] inputValues = equation.split("\\+");
+			int outputValue = Integer.valueOf(inputValues[0]) + Integer.valueOf(inputValues[1]);
+			calculationOutput = String.valueOf(outputValue);
+			System.out.println(calculationOutput);
+		}
+		//To Do - other types of calculation
+	}
+
+	public String getResult() {
+		return calculationOutput;
+	}
+}
+
+
+
